23ce2f03b8795d7644c0fbe4a1b31fb21591123f
unknown <ntninitin@gmail.com>
2018-11-06 04:21:31 +0530

    added

---
+++ Shoot_the_Circle/Assets/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    private bool gameHasEnded = false;
+
+    public Rotator rotator;
+    public Spawner spawner;
+
+    public Animator animator;
+
+    public void EndGame()
+    {
+        if (gameHasEnded)
+            return;
+
+        rotator.enabled = false;
+        spawner.enabled = false;
+
+        Debug.Log("ENd");
+
+        animator.SetTrigger("EndGame");
+
+        gameHasEnded = true;
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
---
+++ Shoot_the_Circle/Assets/Scripts/Pin.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+
+    private bool isPinned = false;
+
+    public float speed = 20f;
+    public Rigidbody2D rb;
+
+    void Update()
+    {
+        if (!isPinned)
+            rb.MovePosition(rb.position + Vector2.up * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Rotator")
+        {
+            transform.SetParent(col.transform);
+            //Score.PinCount++;
+            col.GetComponent<Rotator>().speed += 10f; //increases speed 
+            //col.GetComponent<Rotator>().speed *= -1f; //inverse the rotation
+            isPinned = true;
+        }
+        else if (col.tag == "Pin")
+        {
+            
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+
+}
