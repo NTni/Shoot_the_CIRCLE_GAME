e6deaabea184f3873b0ff6a46e78effb5dc5e7e0
unknown <ntninitin@gmail.com>
2018-11-06 05:09:00 +0530

    score added

--- Shoot_the_Circle/Assets/Scripts/Spawner.cs
+++ Shoot_the_Circle/Assets/Scripts/Spawner.cs
@@ -7,17 +7,22 @@ public class Spawner : MonoBehaviour
 
     public GameObject pinPrefab;
 
+    public float Score =0;
+
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
             SpawnPin();
+
         }
     }
 
     void SpawnPin()
     {
         Instantiate(pinPrefab, transform.position, transform.rotation);
+        Score++;
+        Debug.Log(Score);
     }
 
 }
