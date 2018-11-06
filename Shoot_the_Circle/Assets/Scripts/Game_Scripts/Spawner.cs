9e645ec1ffde357b6ccc492f1fc8b129d06167b7
unknown <ntninitin@gmail.com>
2018-11-07 01:59:19 +0530

    menu ADDDED

---
+++ Shoot_the_Circle/Assets/Scripts/Game_Scripts/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject pinPrefab;
+
+    public float Score =0;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            SpawnPin();
+
+        }
+    }
+
+    void SpawnPin()
+    {
+        Instantiate(pinPrefab, transform.position, transform.rotation);
+        Score++;
+        Debug.Log(Score);
+    }
+
+}
